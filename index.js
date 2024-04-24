b7890ca717514559034e07347c72426fb609fcf2
Hamza Mughal <163909829+hamzamughal8063@users.noreply.github.com>
2024-04-24 18:37:18 +0500

    Add files via upload
    Guessing_Game

---
+++ index.js
@@ -0,0 +1,13 @@
+import inquirere from "inquirer";
+let roundNumber = Math.floor((Math.random() * 4) + 1);
+let userInput = await inquirere.prompt({
+    type: 'number',
+    name: 'guessedNumber',
+    message: 'Guess a number between 1 -4 '
+});
+if (userInput.guessedNumber === roundNumber) {
+    console.log('Congratulations! You guessed correctly!');
+}
+else {
+    console.log(`Sorry! try next time. correct ans is: ${roundNumber}`);
+}
